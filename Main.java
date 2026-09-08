cd6926f7d2451a89c73f84fc69c31a27207211fd
git config --global LakshanEdirisinghe <Ayomall30@gmail.com>
2026-09-08 16:58:31 +0530

    Show PlaceOrder UI alongside MainMenu on application startup

--- Main.java
+++ Main.java
@@ -1,9 +1,11 @@
 import view.MainMenu;
+import view.PlaceOrder;
 
 class Main{
     public static void main(String[] args) {
 
-        new MainMenu().setVisible(true);;
+        new MainMenu().setVisible(true);
+        new PlaceOrder().setVisible(true);
         
     }
 }
