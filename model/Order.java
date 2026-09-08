9a032241cab9c367f1c3b52a89ebfbc758a99fa4
git config --global LakshanEdirisinghe <Ayomall30@gmail.com>
2026-09-08 13:44:06 +0530

    Implement Order class with getters and setters for orderId, custId, and orderStatus

--- model/Order.java
+++ model/Order.java
@@ -4,6 +4,37 @@ public class Order {
 
     private String orderId;
     private String custId;
-    
-    
+    private OrderStatus orderStatus;
+
+    public Order(String orderId, String custId, OrderStatus orderStatus) {
+        this.orderId = orderId;
+        this.custId = custId;
+        this.orderStatus = orderStatus;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public String getCustId() {
+        return custId;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public void setCustId(String custId) {
+        this.custId = custId;
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+
 }
