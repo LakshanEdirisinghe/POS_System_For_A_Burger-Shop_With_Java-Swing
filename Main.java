b08903a15f970f16c588f0c335bcca6b52ec0251
git config --global LakshanEdirisinghe <Ayomall30@gmail.com>
2026-09-08 15:05:55 +0530

    Initialize MainMenu class with layout and panel setup for the application interface

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
+import view.MainMenu;
+
 class Main{
     public static void main(String[] args) {
+
+        new MainMenu().setVisible(true);;
         
     }
 }
--- view/MainMenu.java
+++ view/MainMenu.java
@@ -1,10 +1,26 @@
 package view;
 
-import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
+import javax.swing.*;
 
 public class MainMenu extends JFrame {
+
+    private JPanel subLeftPanel, subRightPanel;
+
+    public MainMenu(){
+        setTitle("Home Page");
+        setSize(1227, 579);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        setLayout(new GridLayout(1, 2));
+        subLeftPanel = new JPanel(new BorderLayout());
+        subRightPanel = new JPanel(new BorderLayout());
+
+
+    }
+    
+
 
     
 }
