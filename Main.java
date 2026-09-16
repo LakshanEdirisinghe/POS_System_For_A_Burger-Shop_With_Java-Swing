8f082ce809599cdd305e286d1795bfefb25f88bd
git config --global LakshanEdirisinghe <Ayomall30@gmail.com>
2026-09-16 13:14:46 +0530

    Comment out unused window launches in Main class to streamline application startup

--- Main.java
+++ Main.java
@@ -4,12 +4,6 @@ class Main{
     public static void main(String[] args) {
 
         new MainMenu().setVisible(true);
-        // new PlaceOrder().setVisible(true);
-        // new BestCustomer().setVisible(true);
-        // new SearchOrder().setVisible(true);
-        // new SearchCustomer().setVisible(true);
-        // new ViewOrders().setVisible(true);
-        // new UpdateDetails().setVisible(true);
 
         
     }
